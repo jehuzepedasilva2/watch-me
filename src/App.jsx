4e86e2a567c8cbe26137d64b6bb187864add8455
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-03-30 21:29:59 -0700

    initial commit/set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Watch from './Watch.jsx'
+
+function App() {
+  return (
+    <Watch />
+  );
+}
+
+export default App
---
+++ src/Watch.jsx
@@ -0,0 +1,10 @@
+
+// will probably be easier to do with css grid over flex box!
+
+export default function Watch() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
