e4f25726e7abfaa57f197f865bad3f93003e0e28
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-03-30 21:59:44 -0700

    number segment (stick) done

--- src/Watch.jsx
+++ src/Watch.jsx
@@ -1,10 +1,7 @@
-
-// will probably be easier to do with css grid over flex box!
+import NumberSegment from "./NumberSegment.jsx"; /// for testing for now
 
 export default function Watch() {
   return (
-    <div>
-      Hello
-    </div>
+    <NumberSegment />
   );
 }
